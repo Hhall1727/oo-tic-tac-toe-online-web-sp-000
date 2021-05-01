010e429c6790539d92e4dc366778485c4a5c69ba
Halie Hall <halierae17@outlook.com>
2021-05-01 21:37:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,11 @@ def valid_move?(index)
 end 
 
 def turn_count
-  
+  count = 0 
+  @board.each do |space|
+    if space == "X" || space == "O"
+      count += 1 
+    end 
 end 
 
 def current_player
