aba8afbe1ac8938230f88d9795bbb8a0cb4abaf6
Halie Hall <halierae17@outlook.com>
2021-05-01 21:44:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,7 +72,11 @@ def won?
   WIN_COMBINATIONS.find do |win_combo|
     if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
       return win_combo
-    elsif  
+    elsif  (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end 
+   false
+ end
 end 
 
 def full?
